dd412987b8885f916ed5ad8990f5465b2592ca90
qakcn <qakcn.yn@gmail.com>
2013-09-20 02:06:42 +0800

    2013/9/19小更新 对搜索框小小修正

--- index/main.js
+++ index/main.js
@@ -6,14 +6,8 @@ $(function(){
 		}else if(dx==0) {
 			e.preventDefault();
 			var sl=$(document).scrollLeft();
-			if(dy<0 && $(document).width()-sl==$(window).width() || dy>0 && sl==0) {
-				var st=$(document).scrollTop();
-				st = (st-dy*50<0) ? 0 : st-dy*50;
-				$(document).scrollTop(st);
-			}else {
-				sl = (sl-dy*80<0) ? 0 : sl-dy*80;
-				$(document).scrollLeft(sl);
-			}
+			sl = (sl-dy*80<0) ? 0 : sl-dy*80;
+			$(document).scrollLeft(sl);
 		}
 	});
 	
