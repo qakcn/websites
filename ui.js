53077cfd874560ff7d571ccc3bfbe270f1b3e6ae
Chengzhi Deng <qakcnyn@gmail.com>
2014-03-21 15:16:03 +0800

    搜索栏完成
    搜索栏变变变

--- ui.js
+++ ui.js
@@ -4,5 +4,16 @@ $(function(){
 		$('nav').toggleClass('show');
 	});
 
-
+	$('#search').click(function(){
+		$('#menuico').removeClass('show').addClass('hide');
+		$('body > header > h1').addClass('hide');
+		$('nav').removeClass('show');
+		$('#search-bar').addClass('show');
+		$('#keyword').focus();
+	});
+	$(document).click(function(e){
+		if($(e.target).is('header,header *')) return true;
+		$('#search-bar,nav,#menuico').removeClass('show');
+		$('#menuico,body>header>h1').removeClass('hide');
+	});
 });
